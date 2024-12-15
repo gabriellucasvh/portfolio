e65cbaed7c5c06e6b7ea4d1b118fca8003bdf22f
Gabriel Gonçalves <137029563+gabriellucasvh@users.noreply.github.com>
2024-12-14 21:05:10 -0300

    Remoção das animações de Scale da navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="hidden md:flex items-center justify-center sticky top-0 left-0 w-full h-12 md:h-16 z-50 px-4">
       <div className="border border-gray-300 dark:border-gray-700 bg-white dark:bg-zinc-950 rounded-md px-2 py-1 md:px-4 mt-1">
         <ul className="flex items-center justify-center gap-2 md:gap-4 text-sm md:text-base">
-          <li className='hover:scale-105 transition-transform duration-500'>
+          <li>
             <Link
               className="hover:text-orange-600 dark:hover:text-orange-500 transition-colors duration-500 "
               href="#menu"
@@ -14,7 +14,7 @@ const Navbar = () => {
               Home
             </Link>
           </li>
-          <li className='hover:scale-105 transition-transform duration-500'>
+          <li>
             <Link
               className="hover:text-orange-600 dark:hover:text-orange-500 transition-colors duration-500"
               href="#projetos"
@@ -22,7 +22,7 @@ const Navbar = () => {
               Projetos
             </Link>
           </li>
-          <li className='hover:scale-105 transition-transform duration-500'>
+          <li>
             <Link
               className="hover:text-orange-600 dark:hover:text-orange-500 transition-colors duration-500"
               href="#sobre"
@@ -30,7 +30,7 @@ const Navbar = () => {
               Sobre
             </Link>
           </li>
-          <li className='hover:scale-105 transition-transform duration-500'>
+          <li>
             <Link
               className="hover:text-orange-600 dark:hover:text-orange-500 transition-colors duration-500"
               href="#tecnologias"
@@ -38,7 +38,7 @@ const Navbar = () => {
               Tecnologias
             </Link>
           </li>
-          <li className='hover:scale-105 transition-transform duration-500'>
+          <li>
             <Link
               className="hover:text-orange-600 dark:hover:text-orange-500 transition-colors duration-500"
               href="#contato"
