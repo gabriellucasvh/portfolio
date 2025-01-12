fa96726f4d7f0c37b41fed7caf68eba09d221930
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-01-12 07:08:57 -0300

    Remoção das bordas em bottom das skills

--- components/Sections/Sobre.tsx
+++ components/Sections/Sobre.tsx
@@ -22,22 +22,23 @@ const Sobre = () => {
         </div>
         {/* Serviços */}
         <div className='flex flex-col items-center justify-center md:items-start w-full md:w-3/4 lg:w-1/2 mt-5 font-extralight '>
-          <div className='flex flex-col items-center md:items-start border-x dark:border-gray-700 px-4 md:px-6 py-4'>
+          <div className='flex flex-col items-center md:items-start md:border-x dark:border-gray-700 px-4 md:px-6 py-4'>
             <h2 className='text-xl font-semibold flex items-center gap-2'><Flame className='text-orange-600' />Competência Técnica</h2>
-            <p className='border-b dark:border-gray-700 text-center mt-2 md:text-start'>
+            <p className='text-center hyphens-auto mt-2 md:text-start'>
               Minha proficiência em HTML, CSS, JavaScript e frameworks como React e Vue.js garante soluções eficazes e adaptáveis para qualquer desafio de desenvolvimento Front-end.
             </p>
           </div>
-          <div className='flex flex-col items-center md:items-start border-x dark:border-gray-700 px-4 md:px-6 py-4'>
+          <div className='h-px w-full bg-gray-700'></div>
+          <div className='flex flex-col items-center md:items-start md:border-x dark:border-gray-700 px-4 md:px-6 py-4'>
             <h2 className='text-xl font-semibold flex items-center gap-2'><ShieldCheck className='text-orange-600' />  Qualidade do Código</h2>
-            <p className='border-b dark:border-gray-700 text-center mt-2 md:text-start'>
+            <p className='text-center hyphens-auto mt-2 md:text-start'>
               Produzo um código limpo, bem documentado e modular, que é fácil de manter e escalar. Testes automatizados e depuração rigorosa garantem a estabilidade e funcionalidade.
             </p>
           </div>
-          <div className=''></div>
-          <div className='flex flex-col items-center md:items-start border-x dark:border-gray-700 px-4 md:px-6 py-4'>
+          <div className='h-px w-full bg-gray-700'></div>
+          <div className='flex flex-col items-center md:items-start md:border-x dark:border-gray-700 px-4 md:px-6 py-4'>
             <h2 className='text-xl font-semibold flex items-center gap-2'><Palette className='text-orange-600' />  Design e Experiência do Usuário</h2>
-            <p className='text-center mt-2 md:text-start'>
+            <p className='text-center hyphens-auto mt-2 md:text-start'>
               Crio interfaces intuitivas, acessíveis e visualmente atraentes. Otimizo para performance e responsividade, assegurando que o aplicativo funcione perfeitamente.
             </p>
           </div>
