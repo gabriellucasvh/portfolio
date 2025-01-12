f55f9ec438c068952d02f4b35e94547ebe59bcbf
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-01-12 06:45:21 -0300

    Ajuste no espaçamento de letras em Sobre e remoção de uma skill

--- components/Sections/Sobre.tsx
+++ components/Sections/Sobre.tsx
@@ -12,28 +12,20 @@ const Sobre = () => {
       <div className='flex flex-col md:flex-row items-center w-full'>
         {/* Sobre */}
         <div className="flex flex-col items-center md:items-start w-full md:w-3/4 lg:w-1/2 mt-5 font-extralight text-base md:text-lg px-4 md:px-6 py-4">
-          <p className="indent-5 text-justify hyphens-auto" style={{ wordSpacing: '-2px' }}>
+          <p className="indent-5 text-justify hyphens-auto" style={{ wordSpacing: '-1.4px' }}>
             Olá, meu nome é Gabriel! Sou estudante de Análise e Desenvolvimento de Sistemas com foco em front-end. Estou sempre em busca de soluções criativas e eficientes para oferecer experiências digitais memoráveis. Tenho como objetivo me tornar um desenvolvedor excelente, combinando técnicas modernas e boas práticas do setor.
           </p>
-          <p className="indent-5 text-justify mt-4 hyphens-auto" style={{ wordSpacing: '-2px' }}>
+          <p className="indent-5 text-justify mt-4 hyphens-auto" style={{ wordSpacing: '-1.4px' }}>
             Sou movido pelo aprendizado constante, acreditando que o crescimento profissional vem do equilíbrio entre prática, conhecimento e inovação.
-          </p>
-          <p className="indent-5 text-justify mt-4 hyphens-auto" style={{ wordSpacing: '-2px' }}>
             Caso queira saber mais sobre meu trabalho, entre em contato! Estou sempre aberto a novos desafios.
           </p>
         </div>
         {/* Serviços */}
-        <div className='flex flex-col items-center md:items-start w-full md:w-3/4 lg:w-1/2 mt-5 font-extralight '>
+        <div className='flex flex-col items-center justify-center md:items-start w-full md:w-3/4 lg:w-1/2 mt-5 font-extralight '>
           <div className='flex flex-col items-center md:items-start border-x dark:border-gray-700 px-4 md:px-6 py-4'>
             <h2 className='text-xl font-semibold flex items-center gap-2'><Flame className='text-orange-600' />Competência Técnica</h2>
             <p className='border-b dark:border-gray-700 text-center mt-2 md:text-start'>
               Minha proficiência em HTML, CSS, JavaScript e frameworks como React e Vue.js garante soluções eficazes e adaptáveis para qualquer desafio de desenvolvimento Front-end.
-            </p>
-          </div>
-          <div className='flex flex-col items-center md:items-start border-x dark:border-gray-700 px-4 md:px-6 py-4'>
-            <h2 className='text-xl font-semibold flex items-center gap-2'><CalendarClock className='text-orange-600' /> Planejamento e Gestão</h2>
-            <p className='border-b dark:border-gray-700 text-center mt-2 md:text-start'>
-              Definir escopos claros e prazos realistas, aliado ao uso de metodologias ágeis, mantém o projeto no caminho certo e facilita a adaptação às mudanças.
             </p>
           </div>
           <div className='flex flex-col items-center md:items-start border-x dark:border-gray-700 px-4 md:px-6 py-4'>
@@ -44,7 +36,7 @@ const Sobre = () => {
           </div>
           <div className='flex flex-col items-center md:items-start border-x dark:border-gray-700 px-4 md:px-6 py-4'>
             <h2 className='text-xl font-semibold flex items-center gap-2'><Palette className='text-orange-600' />  Design e Experiência do Usuário</h2>
-            <p className='border-b dark:border-gray-700 text-center mt-2 md:text-start'>
+            <p className='text-center mt-2 md:text-start'>
               Crio interfaces intuitivas, acessíveis e visualmente atraentes. Otimizo para performance e responsividade, assegurando que o aplicativo funcione perfeitamente.
             </p>
           </div>
