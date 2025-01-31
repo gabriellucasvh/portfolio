9ccf6a21e2d8489a6890c5445896e680c025a3e3
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-01-30 22:59:17 -0300

    Ajuste na altura da navbar e uso de props na mesma

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,54 +1,24 @@
 import Link from 'next/link';
 import { ModeToggle } from '@/components/ModeToggle';
 
+const MenuLinks = [
+  { name: 'Home', href: '#menu' },
+  { name: 'Projetos', href: '#projetos' },
+  { name: 'Sobre', href: '#sobre' },
+  { name: 'Tecnologias', href: '#tecnologias' },
+  { name: 'Contato', href: '#contato' },
+]
 const Navbar = () => {
   return (
     <div className="hidden md:flex items-center justify-center sticky top-0 left-0 w-full h-12 md:h-16 z-50 px-4">
-      <div className="border border-gray-300 dark:border-gray-700 bg-white dark:bg-zinc-950 rounded-md px-2 py-1 md:px-4 mt-1">
-        <ul className="flex items-center justify-center gap-2 md:gap-4 text-sm md:text-base">
-          <li>
-            <Link
-              className="hover:text-orange-600 dark:hover:text-orange-500 transition-colors duration-500 "
-              href="#menu"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="hover:text-orange-600 dark:hover:text-orange-500 transition-colors duration-500"
-              href="#projetos"
-            >
-              Projetos
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="hover:text-orange-600 dark:hover:text-orange-500 transition-colors duration-500"
-              href="#sobre"
-            >
-              Sobre
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="hover:text-orange-600 dark:hover:text-orange-500 transition-colors duration-500"
-              href="#tecnologias"
-            >
-              Tecnologias
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="hover:text-orange-600 dark:hover:text-orange-500 transition-colors duration-500"
-              href="#contato"
-            >
-              Contato
-            </Link>
-          </li>
-          <li>
+      <div className="border border-gray-300 dark:border-gray-700 bg-white dark:bg-zinc-950 rounded-md px-3 py-2 mt-1">
+        <ul className="flex items-center justify-center text-sm md:text-base gap-4">
+          {MenuLinks.map((link) => (
+            <li key={link.name}>
+              <Link className='hover:text-orange-500 transition-all duration-300' href={link.href}>{link.name}</Link>
+            </li>
+          ))}
           <ModeToggle />
-          </li>
         </ul>
       </div>
     </div>
